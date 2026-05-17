0e5d9e039595c748647ec651f032eb7fe73ebb43
alekmiltenov <alekmiltenov@gmail.com>
2026-05-17 23:20:21 +0300

    Initial enums
    Implemented enums for Status and Priority

---
+++ TaskSystem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+enum class Status {
+    Todo,
+    InProgress,
+    Done
+};
+
+enum class Priority {
+    Low,
+    Medium,
+    High
+};
+
