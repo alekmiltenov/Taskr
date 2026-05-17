f6d0f6c018223ddb74154861750bf11bb17f8e7d
alekmiltenov <alekmiltenov@gmail.com>
2026-05-17 23:21:24 +0300

    Implement WorkItem class

--- TaskSystem.cpp
+++ TaskSystem.cpp
@@ -14,3 +14,30 @@ enum class Priority {
     High
 };
 
+
+class WorkItem {
+protected:
+    int id;
+    std::string title;
+    std::string deadline;
+
+public:
+    WorkItem(int id, const std::string& title, const std::string& deadline)
+        : id(id), title(title), deadline(deadline) {}
+
+    virtual ~WorkItem() = default;
+
+    int getId() const {
+        return id;
+    }
+
+    const std::string& getTitle() const {
+        return title;
+    }
+
+    const std::string& getDeadline() const {
+        return deadline;
+    }
+
+    virtual void print(std::ostream& os) const = 0;
+};
